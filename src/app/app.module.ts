168965de8e859dec899981c773382f6880fc848e
kimshuye <suntisuk.go@gmail.com>
2018-05-22 21:08:42 +0700

    add material package to module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import {BrowserAnimationsModule,NoopAnimationsModule} from '@angular/platform-browser/animations';
+import 'hammerjs';
+
+//Material design modules
+import {  
+  MatButtonModule,
+  MatCheckboxModule,
+  MatCardModule,
+  MatGridListModule,
+  MatInputModule,
+  MatDatepickerModule,
+  MatNativeDateModule,
+  MatToolbarModule,
+  MatProgressSpinnerModule,
+  MatTabsModule,
+  MatListModule,
+  MatIconModule,
+
+ } from '@angular/material';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
@@ -13,8 +34,44 @@ import { environment } from '../environments/environment';
   ],
   imports: [
     BrowserModule,
+
+    FormsModule,
+    ReactiveFormsModule,
+    BrowserAnimationsModule,
+    NoopAnimationsModule,
+
     AppRoutingModule,
     // ServiceWorkerModule.register('/ngsw-worker.js', { enabled: environment.production }),
+
+
+    MatButtonModule,
+    MatCheckboxModule,
+    MatCardModule,
+    MatGridListModule,
+    MatInputModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatToolbarModule,
+    MatProgressSpinnerModule,
+    MatTabsModule,
+    MatListModule,
+    MatIconModule,
+
+  ],
+  exports: [
+
+    MatButtonModule,
+    MatCheckboxModule,
+    MatCardModule,
+    MatGridListModule,
+    MatInputModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatToolbarModule,
+    MatProgressSpinnerModule,
+    MatTabsModule,
+    MatListModule,
+    MatIconModule,
 
   ],
   providers: [],
